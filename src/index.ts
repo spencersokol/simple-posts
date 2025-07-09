bf9ea5db10637f733a873afc4c2c30c136dc938f
Spencer Sokol <spencer@idkwtm.com>
2025-07-09 16:39:30 -0400

    SimplePost export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { ISimplePost, ISimplePostMetaData } from "./post.types"
+import type { SimplePost, ISimplePost, ISimplePostMetaData } from "./post.types"
 import { SimplePostsProvider, useSimplePostsContext } from "./post-context"
 import type { ISimplePostsContextData, ISimplePostsContextProviderProps } from "./post-context.types"
 import { BaseSimplePostFactory, SimplePostFactory } from "./post-factory"
@@ -6,6 +6,7 @@ import { BaseSimplePostFactory, SimplePostFactory } from "./post-factory"
 export {
     BaseSimplePostFactory,
     SimplePostFactory,
+    SimplePost,
     ISimplePost,
     ISimplePostMetaData,
     ISimplePostsContextData,
