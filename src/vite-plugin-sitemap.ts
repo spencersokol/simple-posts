2047bcfed5e6a0ef748c49a1339310474ba479f9
Spencer Sokol <spencer@idkwtm.com>
2025-07-16 18:34:05 -0400

    Sitemap generation

--- src/vite-plugin-sitemap.ts
+++ src/vite-plugin-sitemap.ts
@@ -2,19 +2,50 @@ import { ResolvedConfig } from "vite";
 import { ISimplePost } from "./simple-post.types";
 import { ISimplePostOptions } from "./vite-plugin.types";
 import path from "path";
-import { writeFileSync } from "fs";
+import { createWriteStream } from "fs";
+import { SitemapStream } from 'sitemap';
 
 export default function GenerateSitemap(config: ResolvedConfig, options: ISimplePostOptions, content: ISimplePost[]) {
 
     // set defaults for empty options
     const {
         outputDir = path.join(config.root, '/public'),
-        pretty = false,
-        sitemapFileName = 'rss.xml'
+        additionalPostTypes : postTypes = [],
+        sitemapFileName = 'sitemap.xml',
+        rootUrl
     } = options;
 
-    let xml = '';
+    const postTypePrefixes : Record<string, string> = {};
 
-    writeFileSync(`${outputDir}/${sitemapFileName}`, (pretty) ? JSON.stringify(xml, null, 4) : JSON.stringify(xml));
+    // Creates a sitemap object given the input configuration with URLs
+    const sitemap = new SitemapStream({ hostname: rootUrl });
+
+    const writeStream = createWriteStream(path.join(outputDir, sitemapFileName));
+    sitemap.pipe(writeStream);
+
+    for (let i = 0; i < content.length; i++) {
+        
+        const item = content[i];
+
+        if (!postTypePrefixes.keys?.includes(item.type)) {
+            const type = postTypes.find((type) => type.name === item.type);
+            if (!type)
+                continue;
+            postTypePrefixes[item.type] = type.prefix;
+        }
+        
+        const prefix = postTypePrefixes[item.type] ? '/' + postTypePrefixes[item.type] : '';
+        const permalink = `${prefix}/${item.slug}/`;
+
+        sitemap.write(permalink);
+
+    }
+
+    sitemap.end();
+
+    console.log(`Generated sitemap.`);
+    console.log(path.join(outputDir, sitemapFileName));
+
+    //writeFileSync(path.join(outputDir, sitemapFileName), xml);
     
 }
